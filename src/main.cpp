d61ded0a320a6776bfbe71127e9def52cbec2c60
tthaneyaa <tthaneya@gmail.com>
2020-09-09 18:24:53 -0300

    mudei

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,10 @@
 
 int main(){
 	std::cout << "Hello World" << std::endl;
+	std::cout << "Hello World"
+			  << "Hello World" 
+			  << std::endl;
+
+	int nInteiro = 5;
+	std::cout << "o numero inteiro é:" << nInteiro << std::endl;
 }
